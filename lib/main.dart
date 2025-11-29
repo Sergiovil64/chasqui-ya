3541498ab94c28b2cf68b1ddb0f84aa82b0a6266
Sergio Villafan <villafan815@gmail.com>
2025-11-29 11:09:08 -0400

    CRUD Restaurantes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:chasqui_ya/config/app_theme.dart';
+import 'package:chasqui_ya/ui/restaurant/restaurant_home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -13,7 +15,10 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      // home: const RestaurantHomeScreen(),
       home: Scaffold(body: Center(child: Text('Hello World!'))),
     );
   }
